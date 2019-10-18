fa7faf2eba3a8f96bbb1771f3397e99118b9536c
Adrien Czerny <adrien.czerny@gmail.com>
2019-10-18 12:15:13 +0100

    Updating i3, vim

---
+++ .i3/init-i3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ln -sf ~/dotfiles/.i3/config ~/.config/i3/config
--- .vim/init-vimrc.sh
+++ .vim/init-vimrc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ln -sf ~/dotfiles/.vim/.vimrczero ~/.vimrc
-ln -sf ~/dotfiles/.vim/.vimrccolor_deus ~/.vimrccolor
+ln -sf ~/dotfiles/.vim/.vimrccolor_tomorrow_night ~/.vimrccolor
